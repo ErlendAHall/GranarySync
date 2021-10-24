8a9dc3eb460e9c64142c3e9d4d4b27f248344625
Erlend Andreas Hall <ehall@equinor.com>
2021-10-24 15:53:19 +0200

    logger now appends log files to a daily log file

--- logger.js
+++ logger.js
@@ -1,8 +1,27 @@
 import { utils } from "./util.js";
 
-export let logger = {
-  logPath: "/var/log/granarysync",
+const LOGPATHS = {
+  prod: "/var/log/granarysync",
+  debug: "/granary/edev/GranarySync/playground/log"
+}
+Object.freeze(LOGPATHS)
 
+export let logger = {
+  logPath: utils.getEnvironment() === "debug" ? LOGPATHS.debug : LOGPATHS.prod,
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
   /**
    * Creates a single log line entry.
    * @param {"exception"|"rsync"|"azure"|"runtime"} type
@@ -11,9 +30,9 @@ export let logger = {
    */
   createLogEntry: function createLogEntry(type, message) {
     if (Reflect.has(utils, "encodeText")) {
-      let dateToday = new Date().toUTCString();
-      let entry = `[${type.toLocaleUpperCase()}] ${message} ${dateToday}\n`;
-      return utils.encodeText(entry);
+      let dateToday = new Date().toUTCString()
+      let entry = `[${type.toLocaleUpperCase()}] ${message} ${dateToday}\n`
+      return utils.encodeText(entry)
     }
   },
 
@@ -24,18 +43,28 @@ export let logger = {
    * @returns {Promise<void>} void
    */
   writeToLog: async function writeToLogBasedOnType(type, message) {
-    let dateToday = new Date().toISOString();
+    let dateToday = new Date()
+    let formattedDate = `${dateToday.getDate()}-${
+      this.months[dateToday.getMonth()]
+    }-${dateToday.getFullYear()}`
 
     switch (type) {
       case "exception":
+        await Deno.writeFile(
+          `${this.logPath}/(exception)${formattedDate}.log`,
+          this.createLogEntry(type, message),
+          { append: true }
+        )
+        break
       case "rsync":
       case "azure":
       case "runtime":
         await Deno.writeFile(
-          `${this.logPath}/${dateToday}.log`,
+          `${this.logPath}/${formattedDate}.log`,
           this.createLogEntry(type, message),
-        );
-        break;
+          { append: true }
+        )
+        break
     }
-  },
-};
+  }
+}
